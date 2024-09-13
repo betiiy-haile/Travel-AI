f49364e8bf8db062cc4110b98d176ab7d1474e85
betiiy-haile <betelhemhaile74@gmail.com>
2024-09-13 21:58:10 +0300

    build errors

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from 'react';
-import Link from 'next/link';
 import { signInWithEmail, googleLogin } from '@/actions/auth';  // Add your auth actions here
 
 const Page = () => {
